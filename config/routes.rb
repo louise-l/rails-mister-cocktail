b03489da076e730cf05975ee389d5bff390235a7
louise longle <louise.longle@gmail.com>
2020-11-20 12:09:22 +0100

    root is changed

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,22 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :cocktails
+  root to: "cocktails#index"
+  resources :cocktails do
+    resources :doses, only: [:new, :create]
+  end
+  resources :doses, only: :destroy
 end
 
 
-            #        Prefix Verb   URI Pattern                          Controller#Action
-            #     cocktails GET    /cocktails(.:format)                  cocktails#index
-            #               POST   /cocktails(.:format)                  cocktails#create
-            #  new_cocktail GET    /cocktails/new(.:format)              cocktails#new
-            # edit_cocktail GET    /cocktails/:id/edit(.:format)         cocktails#edit
-            #      cocktail GET    /cocktails/:id(.:format)              cocktails#show
-            #               PATCH  /cocktails/:id(.:format)              cocktails#update
-            #               PUT    /cocktails/:id(.:format)              cocktails#update
-            #               DELETE /cocktails/:id(.:format)              cocktails#destroy
+#                   Prefix Verb   URI Pattern                                       Controller#Action
+#            cocktail_doses POST   /cocktails/:cocktail_id/doses(.:format)           doses#create
+#         new_cocktail_dose GET    /cocktails/:cocktail_id/doses/new(.:format)       doses#new
+#             cocktail_dose DELETE /cocktails/:cocktail_id/doses/:id(.:format)       doses#destroy
+#                 cocktails GET    /cocktails(.:format)                              cocktails#index
+#                           POST   /cocktails(.:format)                              cocktails#create
+#              new_cocktail GET    /cocktails/new(.:format)                          cocktails#new
+#             edit_cocktail GET    /cocktails/:id/edit(.:format)                     cocktails#edit
+#                  cocktail GET    /cocktails/:id(.:format)                          cocktails#show
+#                           PATCH  /cocktails/:id(.:format)                          cocktails#update
+#                           PUT    /cocktails/:id(.:format)                          cocktails#update
+#                           DELETE /cocktails/:id(.:format)                          cocktails#destroy
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_19_105129) do
+ActiveRecord::Schema.define(version: 2020_11_20_104036) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,8 +37,4 @@ ActiveRecord::Schema.define(version: 2020_11_19_105129) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "doses", "cocktails"
-  add_foreign_key "doses", "ingredients"
 end
-
-
